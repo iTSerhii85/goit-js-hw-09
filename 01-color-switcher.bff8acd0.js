d415e155e20faf44a5d8cfd516b55f1f99592abd
iTSerhii85 <109918837+iTSerhii85@users.noreply.github.com>
2022-12-23 10:32:19 +0000

    Deploying to gh-pages from @ iTSerhii85/goit-js-hw-09@1a49c49cee3629e8b482deafda2c7016b3ac7e04 🚀

--- 01-color-switcher.bff8acd0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");console.log(e),console.log(t),e.addEventListener("click",(function(t){e.disabled=!0,l=setInterval((()=>{console.log(t)}),1e3)})),t.addEventListener("click",(function(t){e.disabled=!1,clearInterval(l)}));let l=null;
-//# sourceMappingURL=01-color-switcher.bff8acd0.js.map
