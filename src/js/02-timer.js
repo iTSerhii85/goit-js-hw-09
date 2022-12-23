d427ef56591e60222662352e8918fa967ff25e6a
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-23 21:35:33 +0200

    update - 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -31,4 +31,27 @@ flatpickr(input, {
       alert("Please choose a date in the future");
     }
     startBtnEl.disabled = false;
+    const restTime = dataSelect.getTime() - dataNow.getTime();
+    convertMs(restTime)
   },});
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+  console.log(convertMs());
