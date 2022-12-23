1bf2430225c808e919dd76f311698dcea2aeebdc
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-23 11:18:47 +0200

    start task -1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  console.log(startBtn);
+  console.log(stopBtn);
+
+  startBtn.addEventListener('click', onClickStart);
+  stopBtn.addEventListener('click', onClickStop);
+
+  let timerId = null;
+
+  function onClickStart(evt) {
+    startBtn.disabled = true;
+    
+    timerId = setInterval(() => {console.log(evt)}, 1000);
+  };
+
+  function onClickStop(evt) {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+  };
