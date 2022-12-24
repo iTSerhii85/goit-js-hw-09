00f9d8dcf7d2a23729878ccc4732bfdf2134b054
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-24 16:48:09 +0200

    start tasck -3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,22 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  
+}
+
+function onInput(evt) {
+  const formData = {
+    delay: formEl.delay.value,
+    step: formEl.step.value,
+    amount: formEl.step.value,
+  };
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
