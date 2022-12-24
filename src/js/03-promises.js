178006c65ee8a20801bb4c688bc6a1e48e6d24e3
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-24 20:33:10 +0200

    make tasck - 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,46 @@
 import throttle from 'lodash.throttle';
+import Notiflix from 'notiflix';
 
 const formEl = document.querySelector('.form');
 
-formEl.addEventListener('input', throttle(onInput, 500));
-formEl.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(evt) {
-  
-}
+formEl.addEventListener('click', onStart);
 
-function onInput(evt) {
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay)
+  })
+  return promise;
+};
+
+function onStart(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "BUTTON") {
+    return;
+  }
   const formData = {
     delay: formEl.delay.value,
     step: formEl.step.value,
-    amount: formEl.step.value,
+    amount: formEl.amount.value,
   };
-}
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  for (let i = 0; i < formData.amount; i+=1) {
+    createPromise(i+1, Number(formData.delay) + i * Number(formData.step))
+    .then(({position, delay}) => {
+      Notiflix.Notify.success(
+        `✅ Fulfilled promise ${position} in ${delay}ms`
+      );
+    })
+    .catch(({position, delay}) => {
+      Notiflix.Notify.failure(
+        `❌ Rejected promise ${position} in ${delay}ms`
+      );
+    })
   }
-}
+};
