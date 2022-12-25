8df612060492bc735cba28184714071494601c39
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-25 13:11:39 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,26 +3,25 @@ import Notiflix from 'notiflix';
 
 const formEl = document.querySelector('.form');
 
-
 formEl.addEventListener('click', onStart);
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
+    let timerId = setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve({position, delay});
+        resolve({ position, delay });
       } else {
-        reject({position, delay});
+        reject({ position, delay });
       }
-    }, delay)
-  })
+    }, delay);
+  });
   return promise;
-};
+}
 
 function onStart(evt) {
   evt.preventDefault();
-  if (evt.target.nodeName !== "BUTTON") {
+  if (evt.target.nodeName !== 'BUTTON') {
     return;
   }
   const formData = {
@@ -30,17 +29,17 @@ function onStart(evt) {
     step: formEl.step.value,
     amount: formEl.amount.value,
   };
-  for (let i = 0; i < formData.amount; i+=1) {
-    createPromise(i+1, Number(formData.delay) + i * Number(formData.step))
-    .then(({position, delay}) => {
-      Notiflix.Notify.success(
-        `✅ Fulfilled promise ${position} in ${delay}ms`
-      );
-    })
-    .catch(({position, delay}) => {
-      Notiflix.Notify.failure(
-        `❌ Rejected promise ${position} in ${delay}ms`
-      );
-    })
+  for (let i = 0; i < formData.amount; i += 1) {
+    createPromise(i + 1, Number(formData.delay) + i * Number(formData.step))
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
-};
+}
